00d7f6441bdcfd98d3f2b115b7f7d26b57f846ef
Samuel99j <samuelkelvin566@gmail.com>
2024-09-17 09:50:37 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
+// Seleciona todos os botões com a classe 'btn-proximo'
 const avanca = document.querySelectorAll('.btn-proximo');
 
+// Adiciona um ouvinte de evento para cada botão
 avanca.forEach(button => {
-    button.addEventListener('click', function(){
-        const atual = document.querySelector('.ativo');
-        const proximoPasso = 'passo-' + this.getAttribute('data-proximo');
-
+    button.addEventListener('click', function() {
+        // Seleciona o passo atual que está ativo
+        const atual = document.querySelector('.passo.ativo');
+        
+        // Obtém o próximo passo a partir do atributo 'data-proximo'
+        const proximoPassoId = 'passo-' + this.getAttribute('data-proximo');
+        const proximoPasso = document.getElementById(proximoPassoId);
+        
+        // Remove a classe 'ativo' do passo atual e a adiciona ao próximo passo
         atual.classList.remove('ativo');
-        document.getElementById(proximoPasso).classList.add('ativo');
-    })
-})
+        proximoPasso.classList.add('ativo');
+    });
+});
